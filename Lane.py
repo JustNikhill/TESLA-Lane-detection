231a4ece5b3e24edf4365c4dc13c118a16f90112
Nikhil Yadav <nkrider27@gmail.com>
2021-07-16 14:17:09 +0530

    Update Lane.py

--- Lane.py
+++ Lane.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
+# Developer - JustNikhill
 
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import os
@@ -7,6 +8,8 @@ import matplotlib.image as mpimg
 from moviepy.editor import VideoFileClip
 import math
 
+
+# Frame masking and region of intreset 
 def interested_region(img, vertices):
     if len(img.shape) > 2: 
         mask_color_ignore = (255,) * img.shape[2]
@@ -37,12 +40,12 @@ def lines_drawn(img, lines, color=[255, 0, 0], thickness=6):
         #2
         img.shape[0] = min(y1,y2,img.shape[0])
     
-    # to prevent errors in challenge video from dividing by zero
+    # preventing errors in video 
     if((len(lane_l) == 0) or (len(lane_r) == 0)):
         print ('no lane detected')
         return 1
         
-    #3
+  #
     slope_mean_l = np.mean(slope_l,axis =0)
     slope_mean_r = np.mean(slope_r,axis =0)
     mean_l = np.mean(np.array(lane_l),axis=0)
@@ -57,7 +60,7 @@ def lines_drawn(img, lines, color=[255, 0, 0], thickness=6):
     x1_r = int((img.shape[0] - mean_r[0][1] - (slope_mean_r * mean_r[0][0]))/slope_mean_r)
     x2_r = int((img.shape[0] - mean_r[0][1] - (slope_mean_r * mean_r[0][0]))/slope_mean_r)
     
-    #6
+  #
     if x1_l > x1_r:
         x1_l = int((x1_l+x1_r)/2)
         x1_r = x1_l
@@ -85,7 +88,7 @@ def lines_drawn(img, lines, color=[255, 0, 0], thickness=6):
     
     cache = next_frame
     
-
+   # pixels to a line 
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
     line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
@@ -130,3 +133,5 @@ def process_image(image):
 	line_image = hough_lines(roi_image, 4, theta, 30, 100, 180)
 	result = weighted_img(line_image, image, α=0.8, β=1., λ=0.)
 	return result
+
+# please give star if you like my project
